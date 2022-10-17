a3f46fd8fb953e2a09e7bc064d3eeb561f7d1864
Nguyen Van Truyen <nguyenvantruyennvt@gmail.com>
2022-10-17 15:17:38 +0700

    create models

---
+++ src/models/message_model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let Schema = mongoose.Schema;
+
+let MessageSchema = new Schema({
+    sender: {
+        id: String,
+        username: String,
+        avatar: String
+    },
+    receiver: {
+        id: String,
+        username: String,
+        avatar: String,
+    },
+    text: String,
+    file: { data: Buffer, contentType: String, filename: String },
+    status: { type: Boolean, default: false },
+    createAt: { type: Number, default: Date.now },
+    updateAt: { type: Number, default: null },
+    deleteAt: { type: Number, default: null }
+
+})
+
+module.exports = mongoose.model('message', MessageSchema)
