c6acae1706416a020562125dd55f2f24c164cf2a
nhdhao22 <hfaonguyen@gmail.com>
2022-10-17 22:00:30 +0700

    add login page

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ const port = 3000;
 // configViewEngine(app);
 
 app.engine("ejs", expressEjsExtend);
+app.use(express.static('./src/public'));
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(methodOverride("_method"));
-
 app.get("/", (req, res) => {
   return res.render("main");
 });
