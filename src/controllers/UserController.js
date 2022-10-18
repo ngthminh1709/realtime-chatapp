acd0bee63673f02a15132945e872338cd32627d5
minhngth2409 <minhngth2409@gmail.com>
2022-10-18 09:20:43 +0700

    fix logic UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,29 +3,31 @@ const { StatusCodes } = require('http-status-codes');
 
 const JsonSearch = require('search-array').default
 
-
-
-const findUserByUserName = async (req, res) => {
-    const { username } = req.query;
-
-    if (!username) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ message: "Please enter keyword to find !!!" });
-    }
-    else {
-        const users = await User.find();
-        const searcher = new JsonSearch(users);
-
-        const usersFind = searcher.query(username);
-
-        if (usersFind && usersFind.length !== 0) {
-            console.log(usersFind)
-            return res.status(StatusCodes.OK).json(usersFind);
+const UserController = {
+    
+    findUserByUserName: async (req, res) => {
+        const { username } = req.query;
+    
+        if (!username) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: "Please enter keyword to find !!!" });
         }
-        else
-            return res.status(StatusCodes.NOT_FOUND).json({ message: `Not found user with keyword ${username} ` })
-
+        else {
+            const users = await User.find();
+            const searcher = new JsonSearch(users);
+    
+            const usersFind = searcher.query(username);
+    
+            if (usersFind && usersFind.length !== 0) {
+                console.log(usersFind)
+                return res.status(StatusCodes.OK).json(usersFind);
+            }
+            else
+                return res.status(StatusCodes.NOT_FOUND).json({ message: `Not found user with keyword ${username} ` })
+    
+        }
+    
     }
-
 }
 
-module.exports = { findUserByUserName }
+
+module.exports = UserController;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { findUserByUserName } = require("../controllers/UserController");
+const UserController = require("../controllers/UserController");
 const router = express.Router();
 
-router.route("/query").get(findUserByUserName);
+router.get('/query', UserController.findUserByUserName)
 
 module.exports = router;
