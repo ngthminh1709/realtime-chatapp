1eb748f9f84b325be370cb69720e38156eec1a1f
minhngth2409 <minhngth2409@gmail.com>
2022-10-18 11:51:06 +0700

    fixe route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const middlewareController = require("../controllers/MiddlewareController");
 const UserController = require("../controllers/UserController");
 const router = express.Router();
 
-router.get('/query', UserController.findUserByUserName)
-router.put('/edit', UserController.updateUserProfie)
+router.get("/query", UserController.findUserByUserName);
+router.put(
+  "/edit/:id",
+  middlewareController.adminVerifyToken,
+  UserController.updateUserProfie
+);
 
 module.exports = router;
