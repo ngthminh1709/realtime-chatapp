ed984c3feebc19a7c8c89edc690fca32dfe28673
Nguyen Van Truyen <nguyenvantruyennvt@gmail.com>
2022-10-18 14:05:42 +0700

    ok

---
+++ src/chanels/index.js
@@ -0,0 +1,10 @@
+const httpServer = require('http').Server;
+const { Server } = require("socket.io");
+
+// const httpServer = http.Server;
+
+const channels = (server) =>{
+
+}
+
+module.exports = channels(httpServer);
